1ce32916d4ef695e61ed338f404fbce195c9654c
AngelDE98 <nuda1998@gmail.com>
2015-06-14 12:43:14 +0200

    Create a base for a Papyros theme.

--- javascripts/themes/papyros.js
+++ javascripts/themes/papyros.js
@@ -1,1 +1,16 @@
-//TODO create a Papyros theme
+window.papyros = {};
+
+papyros.id = "papyros";
+papyros.name = "Papyros";
+papyros.stylesheet = "./stylesheets/themes/papyros.css";
+
+gnow.init = function() {
+  //TODO
+};
+
+papyros.unload = function() {
+  //TODO
+};
+
+window.themes = window.themes || [];
+window.themes.push(papyros);
